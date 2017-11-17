58e6c90f6b202ef24ec0218156ac0eed6c5468bf
Yury Demidenko <demidenko05@gmail.com>
2017-11-17 16:28:28 +0300

    added APK signing

--- maven-central-deploy.sh
+++ maven-central-deploy.sh
@@ -5,9 +5,10 @@
 read -p "Really deploy to maven cetral repository  (yes/no)? "
 
 if ( [ "$REPLY" == "yes" ] ) then
+  read -p "Enter paraphrase to sign APK: "
   ssh-add ~/.ssh/demidenko05git
   ssh-add -l
-  mvn release:clean release:prepare release:perform -B -e | tee maven-central-deploy.log
+  mvn release:clean release:prepare release:perform -Dandroid.release=true -Dsignpass="$REPLY" -B -e | tee maven-central-deploy.log
   ssh-add -D
 else
   echo 'Exit without deploy'
